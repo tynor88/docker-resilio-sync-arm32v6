227290c821d5291072bb0baccc0c19976f1d36ef
tynor88 <tynor88@users.noreply.github.com>
2018-05-27 12:13:30 +0200

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 # build image
-docker build -t tynor88/docker-resilio-sync-arm32v6 .
+docker build -t tynor88/resilio-sync-arm32v6 .
 # test image
-docker run tynor88/docker-resilio-sync-arm32v6 uname -a
+docker run tynor88/resilio-sync-arm32v6 uname -a
